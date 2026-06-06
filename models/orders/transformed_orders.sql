71152905c891e725194fc992de3bd4099073d1dc
apdataengineer.work@gmail.com <apdataengineer.work@gmail.com>
2026-06-06 10:59:23 +0000

    new code

---
+++ models/orders/transformed_orders.sql
@@ -0,0 +1,14 @@
+{{ config(materialized = 'table') }}
+
+with base_table as 
+(
+  select * 
+  from  {{ source('bigquery_mydataset', 'demo') }}
+
+)
+
+
+select * 
+from base_table
+where gender = 'F'
+
